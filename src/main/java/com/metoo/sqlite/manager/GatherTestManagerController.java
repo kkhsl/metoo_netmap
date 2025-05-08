ef8e83924b80d1bc2497d9d8bc7fd27263b4096c
15838152042@189.cn <15838152042@189.cn>
2025-05-08 14:49:14 +0800

    windows

--- src/main/java/com/metoo/sqlite/manager/GatherTestManagerController.java
+++ src/main/java/com/metoo/sqlite/manager/GatherTestManagerController.java
@@ -23,10 +23,12 @@ import com.metoo.sqlite.utils.license.SystemInfoUtils;
 import com.metoo.sqlite.vo.LicenseVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Paths;
 import java.util.*;
 
 @Slf4j
